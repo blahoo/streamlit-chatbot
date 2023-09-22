1b6db6e45cefd4c9b6d1cbda219c42b34890c15e
blahoo <117117475+blahoo@users.noreply.github.com>
2023-09-21 20:20:30 -0400

    update key.py with secrets

--- key.py
+++ key.py
@@ -1,4 +1,6 @@
-OPENAI_API_KEY = 'YOURKEY'
+import streamlit as st
+
+OPENAI_API_KEY = st.secrets["openai_key.api_key"] # used in order to deploy using streamlit and streamlit secrets
 
 def getOpenAiKey():
     return OPENAI_API_KEY
