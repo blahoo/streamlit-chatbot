5536233ec48c8c1c167d41781c6081473c361114
blahoo <117117475+blahoo@users.noreply.github.com>
2023-09-21 20:52:47 -0400

    manage session state

--- context.py
+++ context.py
@@ -1,7 +1,10 @@
 import openai
 from key import getOpenAiKey
+import streamlit as st
 
+st.cache_data.clear()
 list_context = []
+max_context_length = st.secrets["max_context"]
 
 openai.api_key = getOpenAiKey()
 
@@ -9,6 +12,8 @@ list_context.append({"role": "assistant", "content": "Hello World 👋"})
 
 def add(role, message):
     list_context.append({"role": role, "content": message})
+    if len(list_context) > max_context_length:
+        list_context.pop(0)
 
 def get():
     return list_context
