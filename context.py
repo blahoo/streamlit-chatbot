837740dce5c794bc7a9c8362d2a82e337e3c9ee2
blahoo <117117475+blahoo@users.noreply.github.com>
2023-09-21 22:08:05 -0400

    adding session state

--- context.py
+++ context.py
@@ -3,17 +3,18 @@ from key import getOpenAiKey
 import streamlit as st
 
 st.cache_data.clear()
-list_context = []
+if "list_context" not in st.session_state:
+    st.session_state["list_context"] = []
 max_context_length = st.secrets["max_context"]
 
 openai.api_key = getOpenAiKey()
 
-list_context.append({"role": "assistant", "content": "Hello World 👋"})
+st.session_state.list_context.append({"role": "assistant", "content": "Hello World 👋"})
 
 def add(role, message):
-    list_context.append({"role": role, "content": message})
-    if len(list_context) > max_context_length:
-        list_context.pop(0)
+    st.session_state.list_context.append({"role": role, "content": message})
+    if len(st.session_state.list_context) > max_context_length:
+        st.session_state.list_context.pop(0)
 
 def get():
-    return list_context
+    return st.session_state.list_context
